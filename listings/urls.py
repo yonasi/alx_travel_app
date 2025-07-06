93b191a9f184bd0d89363ad934344bff58b63c69
Yonas Mamo <Yonasma416@gmail.com>
2025-07-06 21:52:57 +0300

    some files rearanged again

---
+++ listings/urls.py
@@ -0,0 +1,9 @@
+# alx_travel_app/listings/urls.py
+
+from django.urls import path
+# from . import views # You'll add views later
+
+urlpatterns = [
+    # path('my-api-endpoint/', views.my_view, name='my-api-endpoint'),
+    # Add your app-specific API endpoints here
+]
